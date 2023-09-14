72c653048a55a4e07bc06608961183567ff09df1
SanatPopliNEU <144727527+SanatPopliNEU@users.noreply.github.com>
2023-09-14 13:10:05 -0400

    Add files via upload

---
+++ Lab_1/src/model/Product.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sanatpopli
+ */
+public class Product {
+    
+    private String name;
+    private String description;
+    private String availnum;
+    private String price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailnum() {
+        return availnum;
+    }
+
+    public void setAvailnum(String availnum) {
+        this.availnum = availnum;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    
+    
+    
+}
